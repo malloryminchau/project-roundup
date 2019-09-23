1a05ecb49c17a741fe9c43e153ee7ed93236cf47
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-09-23 02:28:16 +0000

    homepage routing needs specifics added in

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,8 +3,6 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-
-
   $(document).ready(() => {
 // Help FAQ page that toggles down from nav bar or under title to give instructions for how to fill in the form.
     $('#help-desc-button').on('click', (event) => { // button that targets the body to allow for a toggle to be activated
@@ -12,7 +10,7 @@ module.exports = (db) => {
       $('#help-desc-body').slideToggle(); //target location for the toggle description --> toggles the field down
     })
 
-// CREATE NEW BUTTON CLICKED everything else is toggled away (if help is toggled move it up)
+// CREATE NEW BUTTON CLICKED everything else is toggled away (if help is toggled move it up) name field toggled
     $('#new-event-button').on('click', (event) => { //new-event-button is a stand in for now before route id is selected
       event.preventDefault();
       $('#help-desc-body').toggleUp(); //change command to make sure this is toggled UP when this is clicked (default on all stages)
@@ -36,8 +34,35 @@ module.exports = (db) => {
       $('#event-description-field-body').slideToggle();
     })
 
-
+// NEXT BUTTON FROM EVENT FIELD after the event name and desc its toggled up and the time options are toggled down
     $('#next-button-from-event').on('click', (event) => {
+      event.preventDefault();
+      $.ajax({  // adds the event name and desc and location to database
+        url: '/',
+        method: 'POST',
+        data: 'eventInput',
+        success: function() {
+          addNewEvent('fill-in-with-parameters'); //Andrew file/callable function parameters and name here
+        }
+      })
+      $('#event-description-field-body').toggleUp();
+      $('#new-proposal-form-body').slideToggle();
+    })
+
+    $('#next-button-from-proposal-form').on('click', (event) => {
+      event.preventDefault();
+      router.post("/:newURL", (req, res) => {
+        $.ajax({
+          url: `${'url-name-based-on-database'}`,
+          method: 'POST',
+          data: 'proposals',
+          success: function() {
+            addNewProposal('fill-in-parameters');
+          }
+        })
+        //newURL needs to come from SQL query after event is created
+        res.redirect("/:newURL")
+      })
 
     })
   })
