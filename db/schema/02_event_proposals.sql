5abea394b8385ce609a6d534e75d454e1ca52cab
Andrew Duong <duongandrews@gmail.com>
2019-09-23 22:18:24 +0000

    Added new column into people database

--- db/schema/02_event_proposals.sql
+++ db/schema/02_event_proposals.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS event_proposals CASCADE;
 CREATE TABLE event_proposals (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255),
-  guest_id INTEGER REFERENCES people(id) ON DELETE CASCADE,
+  creator_id INTEGER REFERENCES people(id) ON DELETE CASCADE,
   description TEXT,
   location VARCHAR(255),
   url VARCHAR(255)
--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,70 +1,69 @@
+$(document).ready(() => {
+  // Help FAQ page that toggles down from nav bar or under title to give instructions for how to fill in the form.
+  // $('#help-desc-button').on('click', (event) => { // button that targets the body to allow for a toggle to be activated
+  //   event.preventDefault();
+  //   $('#help-desc-body').slideToggle(); //target location for the toggle description --> toggles the field down
+  // })
 
+  // CREATE NEW BUTTON CLICKED everything else is toggled away (if help is toggled move it up) name field toggled
+  $('#create-event-start').on('click', (event) => { //new-event-button is a stand in for now before route id is selected
+    event.preventDefault();
+    console.log("Hello I am working!")
+    // $('#help-desc-body').toggleUp(); //change command to make sure this is toggled UP when this is clicked (default on all stages)
+    $('#name-email-body').slideDown(); //name fill in form slides down
+  })
 
-  $(document).ready(() => {
-// Help FAQ page that toggles down from nav bar or under title to give instructions for how to fill in the form.
-    // $('#help-desc-button').on('click', (event) => { // button that targets the body to allow for a toggle to be activated
-    //   event.preventDefault();
-    //   $('#help-desc-body').slideToggle(); //target location for the toggle description --> toggles the field down
+  // NEXT BUTTON FROM NAME ENTRY FIELD when clicked the name field should toggle away and the new form will toggle down
+  $('#name-email-next').on('click', (event) => {
+    event.preventDefault();
+    //POST REQUEST to database for USERS table
+    const nameInput = [$('#name-field').val(), $('#email-field').val()];
+    $.ajax({
+      url: '/api/name',
+      method: 'POST',
+      data: {
+        nameInput: nameInput
+      }, //willhave multiple values for the database. figure out how this integrates into a query
+      success: function () { //Andrews file/ callable function to input the data into the file
+        $('#name-email-body').slideUp();
+        $('#event-description-body').slideDown();
+      }
+    })
+    //post request for the name field
+    // $('#help-desc-body').toggleUp();
+
+  })
+
+  // NEXT BUTTON FROM EVENT FIELD after the event name and desc its toggled up and the time options are toggled down
+  $('#description-location-next').on('click', (event) => {
+    event.preventDefault();
+    const eventInput = [$('#event-name'), $('#event-description'), $('#event-location')]
+    $.ajax({ // adds the event name and desc and location to database
+      url: '/api/eventdisc',
+      method: 'POST',
+      data: {
+        eventInput: eventInput
+      },
+      success: function () {
+        $('#event-description-body').slideUp();
+        $('#proposal-times-body').slideDown();
+      }
+    })
+
+  })
+
+  $('#proposal-calendar').on('click', (event) => {
+    event.preventDefault();
+    // router.post("/:newURL", (req, res) => {
+    // $.ajax({
+    //   url: `${'url-name-based-on-database'}`,
+    //   method: 'POST',
+    //   data: 'proposals',
+    //   success: function() {
+    //     addNewProposal('fill-in-parameters');
+    //   }
     // })
+    //newURL needs to come from SQL query after event is created
 
-// CREATE NEW BUTTON CLICKED everything else is toggled away (if help is toggled move it up) name field toggled
-    $('#create-event-start').on('click', (event) => { //new-event-button is a stand in for now before route id is selected
-      event.preventDefault();
-      console.log("Hello I am working!")
-      // $('#help-desc-body').toggleUp(); //change command to make sure this is toggled UP when this is clicked (default on all stages)
-      $('#name-email-body').slideDown(); //name fill in form slides down
-    })
-
-// NEXT BUTTON FROM NAME ENTRY FIELD when clicked the name field should toggle away and the new form will toggle down
-    $('#name-email-next').on('click', (event) => {
-      event.preventDefault();
-      //POST REQUEST to database for USERS table
-      const nameInput = [$('#name-field').val(), $('#email-field').val()];
-      $.ajax({
-        url: '/api/name',
-        method: 'POST',
-        data: {nameInput: nameInput}, //willhave multiple values for the database. figure out how this integrates into a query
-        success: function() {  //Andrews file/ callable function to input the data into the file
-          $('#name-email-body').slideUp();
-          $('#event-description-body').slideDown();
-        }
-      })
-      //post request for the name field
-      // $('#help-desc-body').toggleUp();
-
-    })
-
-// NEXT BUTTON FROM EVENT FIELD after the event name and desc its toggled up and the time options are toggled down
-    $('#description-location-next').on('click', (event) => {
-      event.preventDefault();
-      const eventInput = [$('#event-name'), $('#event-description'), $('#event-location')]
-      $.ajax({  // adds the event name and desc and location to database
-        url: '/api/eventdisc',
-        method: 'POST',
-        data: {eventInput: eventInput},
-        success: function() {
-          $('#event-description-body').slideUp();
-          $('#proposal-times-body').slideDown();
-        }
-      })
-
-    })
-
-    $('#proposal-calendar').on('click', (event) => {
-      event.preventDefault();
-      // router.post("/:newURL", (req, res) => {
-        // $.ajax({
-        //   url: `${'url-name-based-on-database'}`,
-        //   method: 'POST',
-        //   data: 'proposals',
-        //   success: function() {
-        //     addNewProposal('fill-in-parameters');
-        //   }
-        // })
-        //newURL needs to come from SQL query after event is created
-
-      })
-    })
-
-
-
+  })
+})
